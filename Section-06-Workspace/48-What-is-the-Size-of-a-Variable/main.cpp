30d9be4f924d3b822473cb751a512910b1e9cf5d
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-05-18 06:00:33 -0500

    Adding 48-What-is-the-Size-of-a-Variable files and directories

---
+++ Section-06-Workspace/48-What-is-the-Size-of-a-Variable/main.cpp
@@ -0,0 +1,71 @@
+/*
+*  Author: Matt Johnson
+*  Subject: sizeof operator
+*  Purpose: 
+*  Date: 05/18/2023 
+* 
+*  Notes:
+*       
+*           - make sure you include climits for integer types
+*           - similar information for floating-point numbers
+*              is contained in <cfloat>
+* 
+ */
+
+#include <iostream>
+#include <climits>          
+
+using namespace std;
+
+int main(void) {
+    
+    cout << "sizeof information " << endl;
+    cout << "============================\n" << endl;
+    
+    cout << "char: " << sizeof(char) << " bytes." << endl;
+    cout << "int: " << sizeof(int) << " bytes." << endl;
+    cout << "unsigned int: " << sizeof(unsigned int) << " bytes." << endl;
+    cout << "short: " << sizeof(short) << " bytes." << endl;
+    cout << "long: " << sizeof(long) << " bytes." << endl;
+    cout << "long long: " << sizeof(long long) << " bytes." << endl;
+    
+    cout << "\n============================\n" << endl;
+    
+    cout << "float: " << sizeof(float) << " bytes." << endl;
+    cout << "double: " << sizeof(double) << " bytes." << endl;
+    cout << "long double: " << sizeof(long double) << " bytes." << endl;
+    
+    // use values defined in <climits>
+    cout << "\n============================\n" << endl;
+    
+    cout << "Minimum values: " << endl;
+    cout << "char: " << CHAR_MIN << endl;
+    cout << "int: " << INT_MIN << endl;
+    cout << "short: " << SHRT_MIN << endl;
+    cout << "long: " << LONG_MIN << endl;
+    cout << "long long: " << LLONG_MIN << endl;
+    
+    cout << "\n============================\n" << endl;
+    
+    cout << "Maximum values: " << endl;
+    cout << "char: " << CHAR_MAX << endl;
+    cout << "int: " << INT_MAX << endl;
+    cout << "short: " << SHRT_MAX << endl;
+    cout << "long: " << LONG_MAX << endl;
+    cout << "long long: " << LLONG_MAX << endl;
+    
+    cout << "\n============================\n" << endl;
+    
+    // sizeof can also be used with variable names
+    cout << "sizeof using variable names: " << endl;
+    int age {21};
+    
+    cout << "age is " << sizeof(age) << " bytes." << endl;
+    cout << "age is " << sizeof age << " bytes.\n" << endl;
+    
+    double wage {22.24};
+    cout << "wage is " << sizeof(wage) << " bytes." << endl;
+    cout << "wage is " << sizeof wage << " bytes.\n" << endl;
+    
+    return 0;
+}
