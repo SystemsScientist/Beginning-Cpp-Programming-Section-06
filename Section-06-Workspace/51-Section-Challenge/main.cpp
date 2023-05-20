6686bc181ecdf37311530d38cff2d9498478d201
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-05-20 06:03:52 -0500

    Adding 51-Section-Challenge files and directories

---
+++ Section-06-Workspace/51-Section-Challenge/main.cpp
@@ -0,0 +1,55 @@
+/*
+*  Author: Matt Johnson
+*  Subject: section challenge
+*  Purpose: 
+*  Date: 05/20/2023 
+* 
+*  Instructions:
+*       
+*           Frank's Carpet Cleaning Service
+*           Charges:
+*                 $25 per small room
+*                 $35 per large room
+*           Sales tax rate is 6 percent
+*           Estimates are valid for 30 days
+* 
+*           Prompt the user for the number of rooms they would like cleaned
+*           and provide an estimate such as:
+* 
+*           Estimate for carpet cleaning service
+*           Number of small rooms: 3
+*           Number of large rooms: 1
+*           Price per small room: $25
+*           Price per large room: $35
+*           Cost: $110
+*           Tax: $6.6
+*           =================================================
+*           Total estimate: $116.6
+*           This estimate is valid for 30 days
+* 
+*           Pseudocode:
+*               Prompt the user to enter the number of small rooms
+*               Prompt the user to enter the number of large rooms
+*               Display number of small rooms
+*               Display number of large rooms
+*               Display price per small room
+*               Display price per large room
+*           
+*               Display cost of rooms: (number of small rooms * price per small room) + (number of large rooms * price per large rooms)
+*               Display tax: (number of small rooms * price per small room * tax rate) + (number of large rooms * price per large room * tax rate)
+*               Display total estimate: [(number of small rooms * price per small room) + (number of large rooms * price per large rooms)]
+*                                                           + [(number of small rooms * price per small room * tax rate) + (number of large rooms * price per large room * tax rate)]
+*               Display estimate expiration time
+*
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+   
+    cout << "Section 06 Challenge Instructions" << endl;
+    
+    return 0;
+}
